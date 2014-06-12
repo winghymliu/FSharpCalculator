ca045fcc351bb4c3ae012a0ee4cb03a147ccd12a
wing liu <winghymliu@gmail.com>
2014-06-12 17:46:04 +0100

    stack push

--- Wing.Brain/Brain.fs
+++ Wing.Brain/Brain.fs
@@ -1,7 +1,16 @@
 ﻿namespace Wing.Brain
 
 module Brain =
+
+    type Stack = StackContents of float list
+
+    let push x stack =
+        let (StackContents contents) = stack
+        let newContents = x::contents
+        (StackContents newContents)
+        
     let inline public Square x = x * x;
 
     let Add x y =
         x + y
+
--- Wing.Calculator.Tests/CalculatorTests.fs
+++ Wing.Calculator.Tests/CalculatorTests.fs
@@ -11,6 +11,13 @@ module module1 =
 
         let WorkOnList x = List.map (fun (x) -> x.ToString()) x;
         let lame x = List.head x;
+           
+        [<Test>] member test.
+            Stack()=
+                StackContents [] |> should equal (StackContents []);
+                StackContents [1.0;2.0;3.0;] |> should equal (StackContents [1.0;2.0;3.0;]);
+                push 1.0 (StackContents []) |> should equal (StackContents [1.0]);
+                push 1.0 (StackContents [2.0]) |> should equal (StackContents [1.0;2.0]);
 
         [<Test>] member test.
          Square ()=
@@ -21,4 +28,8 @@ module module1 =
 
         [<Test>] member test.
           Add () = 
-            Add 0 0 |> should equal 0
+            Add 0 0 |> should equal 0;
+            Add 0 1 |> should equal 1;
+            Add 1 1 |> should equal 2;
+            Add 2 1 |> should equal 3;
+            Add 1 2 |> should equal 3;
