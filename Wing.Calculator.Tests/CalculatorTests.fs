c3111a5dd97a7f20c2fcd582d0b670b37499a280
wing liu <winghymliu@gmail.com>
2014-07-03 20:31:51 +0100

    renamed stack tests

--- Wing.Calculator.Tests/CalculatorTests.fs
+++ Wing.Calculator.Tests/CalculatorTests.fs
@@ -13,15 +13,24 @@ module module1 =
         let lame x = List.head x;
            
         [<Test>] member test.
-            ``Stack``()=
+            ``Stackcontents can create an empty stack``()=
                 StackContents [] |> should equal (StackContents []);
+        
+        [<Test>] member test.
+            ``Stackcontents can create a stack with values``()=
                 StackContents [1.0;2.0;3.0;] |> should equal (StackContents [1.0;2.0;3.0;]);
+        [<Test>] member test.
+            ``We can push a float into an empty stack content to give a stackcontent with one float``()=
                 push 1.0 (StackContents []) |> should equal (StackContents [1.0]);
+        [<Test>] member test.
+            ``We can push a float into a stack content with values to give a stackcontent with the same values and one float at the head``()=
                 push 1.0 (StackContents [2.0]) |> should equal (StackContents [1.0;2.0]);
 //                pop (StackContents []) |> ignore |> should throw typeof<Wing.Brain.Brain.StackOverflowException>
+        [<Test>] member test.
+                ``We can pop a float from a stack content with values to give a stackcontent with the same values minus one float at the head``()=
                   pop (StackContents [2.0; 1.0]) |> should equal (2.0, StackContents [1.0]);
                   pop (StackContents [2.0]) |> should equal (2.0, StackContents List.empty<float>);
-//
+
         [<Test>] member test.
             Eval()=
                Eval (StackContents [1.0;])( StackContents []) |> should equal 1.0;
