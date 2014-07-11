c394468dbf3be587fbb1d2d3959deb3f94cd9693
wing liu <winghymliu@gmail.com>
2014-07-11 11:15:55 +0100

    more tests and expected exceptions

--- Wing.Calculator.Tests/CalculatorTests.fs
+++ Wing.Calculator.Tests/CalculatorTests.fs
@@ -8,10 +8,7 @@ module module1 =
     
     [<TestFixture>]
     type ``Stack Tests`` ()=
-
-        let WorkOnList x = List.map (fun (x) -> x.ToString()) x;
-        let lame x = List.head x;
-           
+                       
         [<Test>] member test.
             ``Stackcontents can create an empty stack``()=
                 StackContents [] |> should equal (StackContents []);
@@ -25,15 +22,23 @@ module module1 =
         [<Test>] member test.
             ``We can push a float into a stack content with values to give a stackcontent with the same values and one float at the head``()=
                 push 1.0 (StackContents [2.0]) |> should equal (StackContents [1.0;2.0]);
-//                pop (StackContents []) |> ignore |> should throw typeof<Wing.Brain.Brain.StackOverflowException>
+        [<Test>][<ExpectedException>] member test.
+                ``We can't pop a float from an empty stack content and it should throw an exception``()=
+                    pop (StackContents []) |> ignore |> should throw typeof<Wing.Brain.Brain.StackOverflowException>
         [<Test>] member test.
                 ``We can pop a float from a stack content with values to give a stackcontent with the same values minus one float at the head``()=
                   pop (StackContents [2.0; 1.0]) |> should equal (2.0, StackContents [1.0]);
                   pop (StackContents [2.0]) |> should equal (2.0, StackContents List.empty<float>);
 
+    [<TestFixture>]
+    type ``Eval tests`` ()=
         [<Test>] member test.
-            Eval()=
+            ``Evaluating a stack with one result and an empty input stack should just return the result``()=
                Eval (StackContents [1.0;])( StackContents []) |> should equal 1.0;
                Eval (StackContents [5.0;])( StackContents []) |> should equal 5.0;
+        [<Test>] member test.
+            ``Evaluating a result stack with two values and an input stack with a plus operator should return the sum of the two numbers``()=
                Eval (StackContents [5.0;1.0;])( StackContents [Op Plus]) |> should equal 6.0;
+        [<Test>] member test.
+            ``Evaluating an empty result stack and an input stack with two numbers and a plus operator should return the sum of the two numbers``()=               
                Eval (StackContents [])( StackContents [Num 2.0; Num 1.0; Op Plus]) |> should equal 3.0;
